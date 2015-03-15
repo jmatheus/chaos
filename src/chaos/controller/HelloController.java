62c763196ccf6c2d1ec219555738f897b3ce2998
juliomatheus <jmatheus.vaz@gmail.com>
2015-03-15 18:14:59 -0300

    first

---
+++ src/chaos/controller/HelloController.java
@@ -0,0 +1,18 @@
+package chaos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div align='center'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is comming from CrunchifyHelloWorld.java **********<br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+	
+}
